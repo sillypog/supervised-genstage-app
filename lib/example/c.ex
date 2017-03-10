fb890047305633299df41012f696dc08280baf90
Peter Hastie <phastie@bleacherreport.com>
2017-03-10 14:07:56 -0800

    Step 6.

--- lib/example/c.ex
+++ lib/example/c.ex
@@ -4,12 +4,10 @@ defmodule Example.C do
   ##########
   # Client API
   ##########
-  def start_link(pipeline_name, delay) do
-    # Build the process name from the pipeline and module names
+  def start_link(pipeline_name, delay, max_demand) do
     process_name = Enum.join([pipeline_name, "C"], "")
     IO.puts "Start Example.B as #{process_name}"
-    # Launch the process with the dynamically generated name
-    GenStage.start_link(__MODULE__, [pipeline_name, delay], name: String.to_atom(process_name))
+    GenStage.start_link(__MODULE__, [pipeline_name, delay, max_demand], name: String.to_atom(process_name))
   end
 
 
@@ -17,13 +15,11 @@ defmodule Example.C do
   # Server callbacks
   ##########
 
-  def init([pipeline_name, delay]) do
-    # The producer consumer was also launched with a dynamically
-    # generated name, so we'll need to determine this in order
-    # to subscribe
+  def init([pipeline_name, delay, max_demand]) do
     producer = Enum.join([pipeline_name, "B"], "")
     IO.puts "Subscribing Consumer C to #{producer}"
-    {:consumer, [pipeline_name, delay], subscribe_to: [{String.to_atom(producer), min_demand: 0, max_demand: 1}]}
+    # Subscribe to producer/consumer with the max_demand set for this pipeline
+    {:consumer, [pipeline_name, delay], subscribe_to: [{String.to_atom(producer), min_demand: 0, max_demand: max_demand}]}
   end
 
   def handle_events(events, _from, [pipeline_name, counter]) do
--- lib/example/supervisor.ex
+++ lib/example/supervisor.ex
@@ -1,17 +1,15 @@
 defmodule Example.Supervisor do
   use Supervisor
 
-  def start_link(name, consumer_delay) do
+  def start_link(name, consumer_delay, demand) do
     IO.puts "Example.Supervisor #{name} #{consumer_delay}"
-    Supervisor.start_link(__MODULE__, [name, consumer_delay])
+    Supervisor.start_link(__MODULE__, [name, consumer_delay, demand], name: String.to_atom(name))
   end
 
-  def init([pipeline_name, consumer_delay]) do
-    # Pass the pipeline name to the children so they can build
-    # their names dynamically to avoid collisions
+  def init([pipeline_name, consumer_delay, demand]) do
     children = [
-      worker(Example.B, [pipeline_name, 2]),
-      worker(Example.C, [pipeline_name, consumer_delay])
+      worker(Example.B, [pipeline_name, 2, demand]),
+      worker(Example.C, [pipeline_name, consumer_delay, demand])
     ]
 
     opts = [strategy: :one_for_one, name: pipeline_name]
