4f7b2cde8fcdfecd3d9fd3170c221b5cd14decaa
Peter Hastie <phastie@bleacherreport.com>
2017-03-10 13:58:49 -0800

    Generated project with `mix new supervised-genstage-app --module Palleto --app palleto`.

---
+++ lib/palleto.ex
@@ -0,0 +1,18 @@
+defmodule Palleto do
+  @moduledoc """
+  Documentation for Palleto.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Palleto.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/palleto_test.exs
@@ -0,0 +1,8 @@
+defmodule PalletoTest do
+  use ExUnit.Case
+  doctest Palleto
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
