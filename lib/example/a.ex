0565b6c30a68e20762288ae595aad374081d3703
Peter Hastie <pete@sillypog.com>
2026-02-10 13:37:57 -0800

    Minor changes for modern Elixir.

--- lib/example/a.ex
+++ lib/example/a.ex
@@ -8,16 +8,28 @@ defmodule Example.A do
     GenStage.start_link(__MODULE__, counter, name: __MODULE__)
   end
 
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, opts},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
+
 
   ##########
   # Server callbacks
   ##########
 
+  @impl GenStage
   def init(counter) do
     IO.puts "Initalised Producer A with counter at #{counter}"
     {:producer, counter}
   end
 
+  @impl GenStage
   def handle_demand(demand, counter) when demand > 0 do
     IO.puts "Producer A handling demand of #{demand} with #{counter}"
     events = Enum.to_list(counter..counter+demand-1)
--- lib/example/supervisor.ex
+++ lib/example/supervisor.ex
@@ -6,13 +6,24 @@ defmodule Example.Supervisor do
     Supervisor.start_link(__MODULE__, [name, consumer_delay, demand], name: String.to_atom(name))
   end
 
+  def child_spec([name | _] = opts) do
+    %{
+      id: name,
+      start: {__MODULE__, :start_link, opts},
+      type: :supervisor,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
+
+  @impl Supervisor
   def init([pipeline_name, consumer_delay, demand]) do
     children = [
-      worker(Example.B, [pipeline_name, 2, demand]),
-      worker(Example.C, [pipeline_name, consumer_delay, demand])
+      {Example.B, [pipeline_name, 2, demand]},
+      {Example.C, [pipeline_name, consumer_delay, demand]}
     ]
 
     opts = [strategy: :one_for_one, name: pipeline_name]
-    supervise(children, opts)
+    Supervisor.init(children, opts)
   end
 end
